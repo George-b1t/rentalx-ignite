2e207008a898c7cdc7592ec1a5e1df337d82a35b
George-b1t <george.soares509@gmail.com>
2022-03-03 13:36:09 -0300

    Criando repositorio de especificacao

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,22 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specifitaion already exists!");
+    }
+
+    this.specificationRepository.create({ name, description });
   }
 }
 
