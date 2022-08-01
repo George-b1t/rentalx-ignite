3ab0c1be990eb561b15499211a2df061f1404b76
George-b1t <george.soares509@gmail.com>
2022-07-31 23:34:01 -0300

    Criando migration de categoria

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,3 @@
-import { DataSource } from "typeorm";
+import { createConnection } from "typeorm";
 
-const dataSource = new DataSource({
-  type: "postgres",
-  host: "database",
-  username: "docker",
-  password: "rentalx_pass_db",
-  database: "rentalx",
-  port: 5432,
-  migrations: ["./src/database/migrations/*.ts"],
-  entities: ["./src/modules/**/entities/*.ts"],
-});
-
-dataSource.initialize();
+createConnection();
